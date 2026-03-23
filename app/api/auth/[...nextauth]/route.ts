8684b75c67f39170967da377f7fd597e35fd33b7
Your Name <you@example.com>
2026-03-23 18:50:00 +0100

    Fix: Revert edge runtime for nextauth (build fix) and set prisma engineType to wasm to fix fs.readdir error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,8 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const runtime = 'edge';
-
 import { prisma } from "@/lib/prisma";
+
 import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
